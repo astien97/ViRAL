05de29fb324459dd873ff44fdb150d567d0d0952
Christopher Tam <christophertam@brandeis.edu>
2019-11-07 18:26:29 -0500

    clean scene to test UI

---
+++ ViRAL Exhibit/Assets/Scripts/DoTriggerCheck.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoTriggerCheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OVRInput.Update();
+        Debug.Log(OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.LTouch));
+    }
+}
